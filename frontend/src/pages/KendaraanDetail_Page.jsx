b8cf2267be365d971371cca357485c302fb352dc
faturarkansy <faturarkansyawalva@gmail.com>
2025-02-18 14:51:20 +0700

    Update KendaraanDetail_Page.jsx

--- frontend/src/pages/KendaraanDetail_Page.jsx
+++ frontend/src/pages/KendaraanDetail_Page.jsx
@@ -6,18 +6,19 @@ import { faPencil, faTrash } from "@fortawesome/free-solid-svg-icons";
 import swal from "sweetalert";
 import { MapContainer, TileLayer, Marker, Popup, Polyline } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import coordinatesData from "../components/Coordinates.json";
-import L from "leaflet";
-
+import coordinatesData from "../components/Coordinates4.json";
+import Arrow from "/images/arrow.svg";
 import { API_URL } from "../utils/constants";
 import { Button, Modal } from "flowbite-react";
+import { useMap } from "react-leaflet";
 
-// delete L.Icon.Default.prototype._getIconUrl;
-// L.Icon.Default.mergeOptions({
-//     iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-//     iconUrl: require("leaflet/dist/images/marker-icon.png"),
-//     shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
-// });
+function MapUpdater({ coordinates }) {
+    const map = useMap();
+    useEffect(() => {
+        map.setView([coordinates.lat, coordinates.lng], map.getZoom(), { animate: true });
+    }, [coordinates, map]);
+    return null;
+}
 
 export default function KendaraanDetail() {
     const { id } = useParams();
@@ -33,6 +34,8 @@ export default function KendaraanDetail() {
     const [currentIndex, setCurrentIndex] = useState(0);
     const [isTracking, setIsTracking] = useState(false);
     const [progress, setProgress] = useState(0);
+    const [isSortDate, setIsSortDate] = useState(false);
+    const [polylinePositions, setPolylinePositions] = useState([]);
 
 
     useEffect(() => {
@@ -93,15 +96,21 @@ export default function KendaraanDetail() {
         const newIntervalId = setInterval(() => {
             setCurrentIndex((prevIndex) => {
                 if (prevIndex < trackingData.length - 1) {
-                    setProgress(((prevIndex + 1) / trackingData.length) * 100);
-                    return prevIndex + 1;
+                    const nextIndex = prevIndex + 1;
+                    const nextCoordinates = trackingData[nextIndex];
+
+                    // Update posisi marker dan polyline
+                    setCoordinates({ lat: nextCoordinates.lat, lng: nextCoordinates.lng });
+                    setPolylinePositions((prev) => [...prev, nextCoordinates]);
+
+                    return nextIndex;
                 } else {
                     clearInterval(newIntervalId);
                     setIsTracking(false);
                     return prevIndex;
                 }
             });
-        }, 1000);
+        }, 250); // Update posisi setiap 0,25 detik
 
         setIntervalId(newIntervalId);
     };
@@ -114,13 +123,25 @@ export default function KendaraanDetail() {
         }
     };
 
+    const handleSortDate = () => {
+        setIsSortDate(!isSortDate);
+    };
+
 
 
     return (
         <div className="w-[85vw] lg:w-full min-h-full ">
             {/* Header */}
-            <div className="py-5 px-4 w-auto bg-white flex items-center justify-between cursor-pointer mt-5 border-y-2 border-gray-200">
-                <div className="flex items-center gap-2 lg:gap-5">
+            <div
+                className="py-5 px-4 w-auto bg-white flex items-center justify-between cursor-pointer mt-5 border-y-2 border-gray-200"
+                style={{
+                    backgroundImage: "url('images/ornament header.svg')",
+                    backgroundPosition: "right 5px",
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "175px",
+                }}
+            >
+                <div className="flex items-center gap-2 lg:gap-3">
                     <img
                         src="/images/back.svg"
                         alt="Back"
@@ -134,79 +155,78 @@ export default function KendaraanDetail() {
             </div>
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 m-4">
                 {/* Kontainer Kiri - Detail Kendaraan */}
-                <div className="lg:col-span-1 bg-white p-4 border rounded-lg shadow-md">
-                    <div className="grid grid-cols-2 gap-4">
+                <div className="lg:col-span-1 bg-white p-4 border rounded-lg shadow-md flex flex-col justify-between h-full">
+                    <div className="grid grid-cols-2 gap-4 flex-1">
                         <div>
-                            <p className="text-cyan-600 font-bold">No Kendaraaan</p>
+                            <p className="text-cyan-600 text-xs lg:text-base font-bold">No Kendaraaan</p>
                             <p className="text-gray-600 text-xs lg:text-base">{vehicle.no_kendaraan}</p>
                         </div>
                         <div>
-                            <p className="text-cyan-600 font-bold">ID Perangkat</p>
+                            <p className="text-cyan-600 text-xs lg:text-base font-bold">ID Perangkat</p>
                             <p className="text-gray-600 text-xs lg:text-base">{vehicle.id_perangkat}</p>
                         </div>
                         <div>
-                            <p className="text-cyan-600 font-bold">Kategori</p>
+                            <p className="text-cyan-600 text-xs lg:text-base font-bold">Kategori</p>
                             <p className="text-gray-600 text-xs lg:text-base">{vehicle.kategori}</p>
                         </div>
                         <div>
-                            <p className="text-cyan-600 font-bold">Driver</p>
+                            <p className="text-cyan-600 text-xs lg:text-base font-bold">Driver</p>
                             <p className="text-gray-600 text-xs lg:text-base">{vehicle.driver}</p>
                         </div>
                         <div>
-                            <p className="text-cyan-600 font-bold">Update Terakhir</p>
+                            <p className="text-cyan-600 text-xs lg:text-base font-bold">Update Terakhir</p>
                             <p className="text-gray-600 text-xs lg:text-base">{vehicle.last_update}</p>
                         </div>
                         <div>
-                            <p className="text-cyan-600 font-bold">Operator</p>
+                            <p className="text-cyan-600 text-xs lg:text-base font-bold">Operator</p>
                             <p className="text-gray-600 text-xs lg:text-base">{vehicle.operator}</p>
                         </div>
                         <div>
-                            <p className="text-cyan-600 font-bold">No Telepon</p>
+                            <p className="text-cyan-600 text-xs lg:text-base font-bold">No Telepon</p>
                             <p className="text-gray-600 text-xs lg:text-base">{vehicle.no_telepon}</p>
                         </div>
                         <div>
-                            <p className="text-cyan-600 font-bold">Mesin</p>
+                            <p className="text-cyan-600 text-xs lg:text-base font-bold">Mesin</p>
                             <p className="text-gray-600 text-xs lg:text-base">
                                 {vehicle.detail?.engineStatus || ""}
                             </p>
                         </div>
                         <div>
-                            <p className="text-cyan-600 font-bold">Kondisi</p>
+                            <p className="text-cyan-600 text-xs lg:text-base font-bold">Kondisi</p>
                             <p className="text-gray-600 text-xs lg:text-base">{vehicle.detail?.status || ""}</p>
                         </div>
                         <div>
-                            <p className="text-cyan-600 font-bold">Kecepatan</p>
+                            <p className="text-cyan-600 text-xs lg:text-base font-bold">Kecepatan</p>
                             <p className="text-gray-600 text-xs lg:text-base">{vehicle.detail?.speed || ""}</p>
                         </div>
                         <div>
-                            <p className="text-cyan-600 font-bold">Longitude</p>
+                            <p className="text-cyan-600 text-xs lg:text-base font-bold">Longitude</p>
                             <p className="text-gray-600 text-xs lg:text-base">
                                 {vehicle.detail?.coordinate.longitude || ""}
                             </p>
                         </div>
                         <div>
-                            <p className="text-cyan-600 font-bold">Latitude</p>
+                            <p className="text-cyan-600 text-xs lg:text-base font-bold">Latitude</p>
                             <p className="text-gray-600 text-xs lg:text-base">
                                 {vehicle.detail?.coordinate.latitude || ""}
                             </p>
                         </div>
                         <div>
-                            <p className="text-cyan-600 font-bold">Jenis Paket</p>
+                            <p className="text-cyan-600 text-xs lg:text-base font-bold">Jenis Paket</p>
                             <p className="text-gray-600 text-xs lg:text-base">{vehicle.subscription?.type || ""}</p>
                         </div>
                         <div>
-                            <p className="text-cyan-600 font-bold">Paket Berakhir</p>
+                            <p className="text-cyan-600 text-xs lg:text-base font-bold">Paket Berakhir</p>
                             <p className="text-gray-600 text-xs lg:text-base">
                                 {vehicle.subscription?.expired_date || ""}
                             </p>
                         </div>
                     </div>
+
                     <hr className="border-gray-300 my-4" />
-                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-3 my-3 justify-center items-center">
-                        <Button
-                            onClick={() => navigate(`/kendaraan/${vehicle.id}/edit`)}
-                            className="bg-cyan-600"
-                        >
+
+                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-3 mt-auto">
+                        <Button onClick={() => navigate(`/kendaraan/${vehicle.id}/edit`)} className="bg-cyan-600">
                             <p>
                                 <FontAwesomeIcon icon={faPencil} /> Edit Data
                             </p>
@@ -216,10 +236,9 @@ export default function KendaraanDetail() {
                                 <FontAwesomeIcon icon={faTrash} /> Hapus Kendaraan
                             </p>
                         </Button>
-
-
                     </div>
                 </div>
+
                 {/* Kontainer Kanan - Video dan Tracking */}
                 <div className="lg:col-span-2 flex flex-col gap-4">
                     {/* Kontainer Video Live Kamera */}
@@ -234,9 +253,40 @@ export default function KendaraanDetail() {
                     {/* Kontainer Tracking Jalan */}
                     <div className="grid grid-rows-3 lg:grid-cols-3 lg:grid-rows-1 bg-white p-4 rounded-lg shadow-md min-h-[300px] gap-4">
                         {/* Kontainer Judul */}
-                        <div className="row-span-1 lg:col-span-1 flex flex-col items-center justify-center text-lg font-bold text-cyan-600 rounded-lg p-6 space-y-4">
+                        <div className="row-span-1 lg:col-span-1 flex flex-col items-center justify-center text-lg font-bold text-cyan-600 rounded-lg py-6 px-14 space-y-4">
                             {/* Judul */}
                             <h2 className="text-xl text-cyan-600">Tracking Jalan</h2>
+
+                            {/* Sort Date */}
+                            <div className="w-full relative">
+                                <div className="bg-gray-200 text-center text-gray-700 text-xs font-mono rounded-md py-2 px-3 cursor-pointer flex items-center justify-center gap-2" onClick={handleSortDate}>
+                                    <div className="mr-4">14 Oct 2024</div>
+                                    <img
+                                        src={Arrow}
+                                        alt="Arrow"
+                                        className={`w-3 h-3 transition-transform duration-500 ${isSortDate ? "rotate-90" : "rotate-[270deg]"}`}
+                                    />
+                                </div>
+                                {isSortDate && (
+                                    <div className="absolute left-0 mt-2 bg-white border border-gray-300 rounded-md shadow-md p-2 z-50 w-full">
+                                        <ul className="text-gray-700 text-xs text-center">
+                                            <li className="py-1 px-2 hover:bg-gray-100 cursor-pointer">13 Oct 2024</li>
+                                            <li className="py-1 px-2 hover:bg-gray-100 cursor-pointer">12 Oct 2024</li>
+                                            <li className="py-1 px-2 hover:bg-gray-100 cursor-pointer">11 Oct 2024</li>
+                                        </ul>
+                                    </div>
+                                )}
+                            </div>
+                            {/* Progress Bar */}
+                            <div className="w-full">
+                                <p className="font-semibold text-center text-gray-700 text-xs mb-2">Progress Bar</p>
+                                <div className="relative w-full h-2 bg-gray-300 rounded-full">
+                                    <div
+                                        className="absolute top-0 left-0 h-2 bg-cyan-500 rounded-full"
+                                        style={{ width: `${progress}%` }}
+                                    ></div>
+                                </div>
+                            </div>
 
                             {/* Button Group */}
                             <div className="flex space-x-4">
@@ -256,27 +306,7 @@ export default function KendaraanDetail() {
                                 </button>
                             </div>
 
-                            {/* Progress Bar */}
-                            <div className="w-full">
-                                <p className="font-semibold text-center text-gray-700 text-xs mb-2">Progress Bar</p>
-                                <div className="relative w-full h-4 bg-gray-300 rounded-full">
-                                    <div
-                                        className="absolute top-0 left-0 h-4 bg-cyan-500 rounded-full"
-                                        style={{ width: `${progress}%` }}
-                                    ></div>
-                                </div>
-                            </div>
 
-                            {/* Longitude & Latitude */}
-                            <div className="w-full bg-gray-200 text-center text-gray-700 text-xs font-mono rounded-md p-2">
-                                {trackingData.length > 0 ? (
-                                    <>
-                                        {trackingData[trackingData.length - 1].lat}, {trackingData[trackingData.length - 1].lng}
-                                    </>
-                                ) : (
-                                    "No Data"
-                                )}
-                            </div>
                         </div>
                         {/* Kontainer Map */}
                         <div className="row-span-2 lg:col-span-2 rounded-md overflow-hidden ">
@@ -287,24 +317,13 @@ export default function KendaraanDetail() {
                                 scrollWheelZoom={true}
                                 className="w-full h-full"
                             >
+                                <MapUpdater coordinates={coordinates} />
                                 <TileLayer
                                     url='https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}&key=AIzaSyCxPsqzrnGiVjmKIBwCDdzAUIBVbBM2Ums'
                                     attribution=''
                                 />
-                                {trackingData.slice(0, currentIndex + 1).map((coord, index) => (
-                                    <Marker key={index} position={[coord.lat, coord.lng]}>
-                                        <Popup>
-                                            Latitude: {coord.lat.toFixed(5)} <br />
-                                            Longitude: {coord.lng.toFixed(5)}
-                                        </Popup>
-                                    </Marker>
-                                ))}
-                                {currentIndex > 0 && (
-                                    <Polyline
-                                        positions={trackingData.slice(0, currentIndex + 1).map((coord) => [coord.lat, coord.lng])}
-                                        color="blue"
-                                    />
-                                )}
+                                <Marker position={coordinates}></Marker>
+                                <Polyline positions={polylinePositions} color="blue" />
                             </MapContainer>
                         </div>
 
